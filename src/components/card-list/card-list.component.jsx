ea251a1bfd6024241056c1b6e185cbb357011f00
Gustavo Tua <gustavo.tua@hotmail.com>
2022-07-04 00:14:51 -0500

    deploy to git

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,23 @@
+
+import Card from "../card/card.component";
+import '../card-list/card-list.styles.css'
+
+
+const CardList = ({monsters}) => (
+
+
+            <div className="card-list">
+                { monsters.map( (monster) =>{ 
+
+                    return(
+                    <Card monster={monster} />
+                    )
+                    })}
+            </div>
+
+
+
+);
+
+
+export default CardList;
